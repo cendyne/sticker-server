f5136204efbb8d9f4c5e199f0ff92a8b7aea3605
Cendyne <cendyne@cendyne.dev>
2022-03-12 12:51:17 -0600

    More logging

--- src/paths.js
+++ src/paths.js
@@ -1,10 +1,14 @@
 const path = require('path');
-let filePath = process.env.FILES_PATH || './files/';
 let staticPath = path.resolve('static');
+
+let filePath = process.env.FILES_PATH || './files/';
+console.log('File path before mod:', filePath)
 if (filePath.endsWith('/')) {
+  console.log('Trimming file path end')
   filePath = filePath.slice(0, -1);
 }
-if (!filePath.startsWith('/')) {
+if (filePath.startsWith('.')) {
+  console.log('Resolving file path')
   filePath = path.resolve(...filePath.split('/'))
 }
 console.log('File path:', filePath)
