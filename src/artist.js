b42fa0c05574d6c548166c248c02546d2ad78c4b
Cendyne <cendyne@cendyne.dev>
2022-03-11 23:35:53 -0600

    Refactor artist stuff

--- src/artist.js
+++ src/artist.js
@@ -1,14 +1,65 @@
 const db = require('./db').knex;
+const debug = require('./debug');
 
 async function findArtistByVanity(vanity) {
-  let result = await db('artist').where('vanity', '=', vanity).select('*');
+  let result = await db('artist').where({vanity}).select('*');
   if (result.length > 0) {
-    let row = result[0];
-    return row
+    debug('Found artist %s', vanity);
+    return result[0];
   }
+  debug('Could not find artist %s', vanity);
   return null;
 }
 
+async function findArtistById(id) {
+  let result = await db('artist').where({id}).select('*');
+  if (result.length > 0) {
+    debug('Found artist %s', id);
+    return result[0];
+  }
+  debug('Could not find artist by id%s', id);
+  return null;
+}
+
+async function updateArtist(id, name, href) {
+  let result = await db('artist').where({id}).update({
+    name,
+    href
+  });
+  if (result > 0) {
+    debug('Successfully updated artist %d', id);
+    return await findArtistById(id);
+  }
+  debug('Failed to update artist %d', id)
+  return null;
+}
+async function insertArtist(vanity, name, href) {
+  let result = await db('artist').returning('*').insert({
+    vanity,
+    name,
+    href
+  });
+  if (result && result.length > 0 && result[0]) {
+    debug('Inserted artist %d', result[0].id)
+  } else {
+    debug('Failed to insert artist %s', result);
+  }
+
+  return result[0];
+}
+
+async function upsertArtist(vanity, name, href) {
+  let artist = await findArtistByVanity(vanity);
+  if (artist) {
+    debug('Upsert found artist %d, therefore updating', artist.id);
+    return await updateArtist(artist.id, name, href);
+  } else {
+    debug('Upsert did not find a artist, therefore inserting');
+    return await insertArtist(vanity, name, href);
+  }
+}
+
 module.exports = {
-  findArtistByVanity
+  findArtistByVanity,
+  upsertArtist,
 }
--- src/stickerFile.js
+++ src/stickerFile.js
@@ -1,47 +1,78 @@
 const db = require('./db').knex;
+const debug = require('./debug');
 
-async function upsertStickerFile(sticker, content_type, size, source, length, md5) {
+async function findStickerFileById(id) {
+  let result = await db('sticker_file').where({id}).select('*');
+  if (result.length > 0) {
+    debug('Found sticker file %d', id);
+    return result[0];
+  }
+
+  return null;
+}
+
+async function findStickerFile(sticker_id, content_type, size) {
   let result = await db('sticker_file').where({
-    sticker_id: sticker.id,
+    sticker_id,
     content_type,
     size
-  }).select('id');
+  }).select('*');
   if (result.length > 0) {
-    let row = result[0];
-    let id = row['id'];
-    // This is an update
-    result = await db('sticker_file').returning('*').where('id', '=', id).update({
-      // sticker_id cannot change
-      // content_type cannot change
-      // size cannot change
-      source,
-      length,
-      md5
-    });
-    if (result > 0) {
-      // Is a row count
-      result = await db('sticker_file').where('id', '=', id).select('*');
-    } else {
-      throw new Error('Expected to update an sticker_file but could not?');
-    }
-    console.log(result);
+    debug('Found sticker file %d %s %d', sticker_id, content_type, size);
     return result[0];
+  }
+  debug('Could not find sticker file %d %s %d', sticker_id, content_type, size);
+  return null;
+}
+
+async function updateStickerFile(id, source, length, md5) {
+  let result = await db('sticker_file').returning('*').where('id', '=', id).update({
+    // sticker_id cannot change
+    // content_type cannot change
+    // size cannot change
+    source,
+    length,
+    md5
+  });
+  if (result > 0) {
+    debug('Successfully updated sticker file %d', id);
+    return await findStickerFileById(id);
+  }
+  debug('Failed to update sticker file %d', id)
+  return null;
+}
+
+async function insertStickerFile(sticker_id, content_type, size, source, length, md5) {
+  let result = await db('sticker_file').returning('*').insert({
+    sticker_id,
+    content_type,
+    size,
+    source,
+    length,
+    md5
+  });
+  if (result && result.length > 0 && result[0]) {
+    debug('Inserted sticker file %d', result[0].id)
   } else {
-    result = await db('sticker_file').returning('*').insert({
-      sticker_id: sticker.id,
-      content_type,
-      size,
-      source,
-      length,
-      md5
-    })
-    console.log(result);
-    return result[0];
+    debug('Failed to insert sticker file %s', result);
+  }
+  return result[0];
+}
+
+async function upsertStickerFile(sticker, content_type, size, source, length, md5) {
+  let stickerFile = await findStickerFile(sticker.id, content_type, size)
+  if (stickerFile) {
+    debug('Upsert found sticker file %d, therefore updating', stickerFile.id);
+    return await updateStickerFile(stickerFile.id, source, length, md5);
+  } else {
+    debug('Upsert did not find a sticker file, therefore inserting');
+    return await insertStickerFile(sticker.id, content_type, size, source, length, md5);
   }
 }
 
-// TODO findStickerFile
+// TODO find sticker files by sticker
 
 module.exports = {
-  upsertStickerFile
+  upsertStickerFile,
+  findStickerFile
 }
